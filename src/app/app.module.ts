b11ebcb0d8ed016703ad35608ed2980f4eaba538
DimitrisBourgiotis <dbourgiotis@convertgroup.com>
2018-05-12 13:04:35 +0300

    reactive form init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
@@ -21,6 +21,7 @@ import { routes } from './routes';
     BrowserModule,
     CommonModule,
     HttpClientModule,
+    ReactiveFormsModule,
     FormsModule,
     RouterModule.forRoot(routes)
   ],
--- src/app/widgets/bug-create-edit/bug-create-edit.component.ts
+++ src/app/widgets/bug-create-edit/bug-create-edit.component.ts
@@ -5,6 +5,8 @@ import { HttpService } from '../../services/http.service';
 import { Router } from '@angular/router';
 import { debounce} from 'rxjs/operators';
 import { timer } from 'rxjs/observable/timer';
+import { FormGroup, FormControl, Validators, AbstractControl } from '@angular/forms';
+
 
 @Component({
   selector: 'codehub-bug-create-edit',
@@ -17,6 +19,32 @@ export class BugCreateEditComponent implements OnInit {
   priorities: number[] = [1, 2, 3];
   reporters: string[] = ['QA', 'PO', 'DEV'];
   statusTypes: string[] = ['Ready for test', 'Done', 'Rejected'];
+  bugForm: FormGroup;
+
+  titleFormControl = new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(25)]);
+  titleFormControlErrorMessage = '';
+  titleFormControlValidationMessages = {
+    required : 'The first name is required',
+    minlength: 'The minlength is 3 characters',
+    maxlength: 'The maxlength is 25 characters'
+  };
+
+  descriptionFormControl = new FormControl('', [Validators.required, Validators.minLength(3)]);
+  descriptionFormControlErrorMessage = '';
+  descriptionFormControlValidationMessages = {
+    required : 'The first name is required',
+    minlength: 'The minlength is 3 characters',
+  };
+
+  priorityFormControl = new FormControl('');
+  reporterFormControl = new FormControl('');
+  reporterFormControlErrorMessage = '';
+  statusFormControl = new FormControl('');
+  statusFormControlErrorMessage = '';
+  statusFormControlValidationMessages = {
+    required : 'Status is required',
+  };
+
 
   constructor(
     private _httpService: HttpService,
@@ -30,17 +58,67 @@ export class BugCreateEditComponent implements OnInit {
       priority: null,
       reporter: null,
       status: null
-
     };
+
+    this.bugForm = new FormGroup({
+      title: this.titleFormControl,
+      description: this.descriptionFormControl,
+      priority: this.priorityFormControl,
+      reporter: this.reporterFormControl,
+      status: this.statusFormControl
+    });
+
+    this.titleFormControl.valueChanges.subscribe(
+      value => {
+        this.titleFormControlErrorMessage = '';
+        if ((this.titleFormControl.touched || this.titleFormControl.dirty) && this.titleFormControl.errors) {
+          this.titleFormControlErrorMessage = Object.keys(this.titleFormControlValidationMessages)
+            .map(c => this.titleFormControlErrorMessage[c]).join(' ');
+        }
+      }
+    );
+
+    this.descriptionFormControl.valueChanges.subscribe(
+      value => {
+        this.descriptionFormControlErrorMessage = '';
+        if ((this.descriptionFormControl.touched || this.descriptionFormControl.dirty) && this.descriptionFormControl.errors) {
+          this.descriptionFormControlErrorMessage = Object.keys(this.descriptionFormControlValidationMessages)
+            .map(c => this.descriptionFormControlErrorMessage[c]).join(' ');
+        }
+      }
+    );
+
+    this.reporterFormControl.valueChanges.subscribe(
+      value => {
+        this.statusFormControlErrorMessage = '';
+        if ((this.reporterFormControl.touched || this.reporterFormControl.dirty) && this.reporterFormControl.value === 'QA') {
+          this.statusFormControl.setValidators(Validators.required);
+        } else if ((this.reporterFormControl.touched || this.reporterFormControl.dirty) && this.reporterFormControl.value !== 'QA') {
+          this.statusFormControl.clearValidators();
+        }
+        this.statusFormControl.updateValueAndValidity();
+      }
+    );
+
+    this.statusFormControl.valueChanges.subscribe(
+      value => {
+        this.statusFormControlErrorMessage = '';
+        if ((this.statusFormControl.touched || this.statusFormControl.dirty) && this.statusFormControl.errors) {
+          this.statusFormControlErrorMessage = Object.keys(this.statusFormControlValidationMessages)
+            .map(c => this.statusFormControlErrorMessage[c]).join(' ');
+        }
+      }
+    );
   }
 
   formSubmit() {
-    this._httpService.postBug(this.bug).pipe(debounce(() => timer(5000))).subscribe(
-      data => {
-        console.log(data);
-        this._router.navigate(['']);
-      },
-      err => console.log(err)
-    );
+    console.log(this.bugForm);
+    // this._httpService.postBug(this.bug).pipe(debounce(() => timer(5000))).subscribe(
+    //   data => {
+    //     console.log(data);
+    //     this._router.navigate(['']);
+    //   },
+    //   err => console.log(err)
+    // );
   }
 }
