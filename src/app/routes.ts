2b4027c5cbf2225e815c21c58e1872f381915679
DimitrisBourgiotis <dbourgiotis@convertgroup.com>
2018-05-12 15:07:39 +0300

    deprecated routes fix

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,6 +1,4 @@
 import { Routes } from '@angular/router';
-import { BugTableComponent } from './features/bug-table/bug-table.component';
-import { BugCreateEditComponent } from './features/bug-create-edit/bug-create-edit.component';
 
 export const routes: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full'},
